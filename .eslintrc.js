8e24d09e3cf55ff291c1c691a91dac375637cb94
sandro-git <sandroazerty@icloud.com>
2023-05-10 16:32:27 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "vue/multi-word-component-names": "off",
+    }
+}
--- app.vue
+++ app.vue
@@ -1,5 +1,85 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav>
+      <div class="image-container">
+        <nuxt-link href="./index.html">
+          <nuxt-img 
+            src="/logo.png" 
+            alt="Le Castell"
+          />
+        </nuxt-link>
+      </div>
+      <div class="onglets">
+        <a href="">Home</a>
+        <a href="./menu_card.html">Menus</a>
+        <a href="./wine_card.html">Vins</a>
+        <a href="./drinks.html">Cocktails et Boissons</a>
+        <a href="./photos.html">Photos</a>
+        <a href="./contact">Contact</a>
+      </div>
+    </nav>
+
+    <header>
+      <h1>Bienvenue</h1>
+      <h4>Ouvert tous les jours à partir de 19h30</h4>
+    </header>
+
+    <main>
+      <div class="slideshow-container">
+        <div class="slide active">
+          <img src="./ressources/salade.jpg">
+          <div class="box">
+            <p>Salade de saison</p>
+          </div>
+        </div>
+                
+        <div class="slide">
+          <img src="./ressources/viande.jpg">
+          <div class="box">
+            <p>Brochette de viandes</p>
+          </div>
+        </div>
+                
+        <div class="slide">
+          <img src="./ressources/dessert.jpg">
+          <div class="box">
+            <p>Crème catalane</p>
+          </div>
+        </div>
+                
+        <a
+          class="prev"
+          onclick="prevSlide()"
+        >&#10094;</a>
+        <a
+          class="next"
+          onclick="nextSlide()"
+        >&#10095;</a>
+      </div>
+    </main>
+
+        
+
+    <footer>
+      <h1>Nos services</h1>
+      <div class="services">
+        <div class="service">
+          <h3>Livraison gratuite</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, alias praesentium sapiente est debitis eos saepe illum, sed dolorum necessitatibus repudiandae tempore id? Sunt adipisci, voluptatum vel veritatis facilis esse!</p>
+        </div>
+        <div class="service">
+          <h3>Paiement en ligne</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, alias praesentium sapiente est debitis eos saepe illum, sed dolorum necessitatibus repudiandae tempore id? Sunt adipisci, voluptatum vel veritatis facilis esse!</p>
+        </div>
+        <div class="service">
+          <h3>Aimé ou remboursé</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, alias praesentium sapiente est debitis eos saepe illum, sed dolorum necessitatibus repudiandae tempore id? Sunt adipisci, voluptatum vel veritatis facilis esse!</p>
+        </div>
+      </div>
+
+      <p id="contact">
+        Contact : 04 68 80 72 08 | &copy; 2023, Le Castell
+      </p>
+    </footer>
   </div>
 </template>

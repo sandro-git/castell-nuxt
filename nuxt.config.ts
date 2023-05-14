b13ca325bfa13244e728b25ed0fe06ccfc4b24d0
sandro-git <sandroazerty@icloud.com>
2023-05-14 16:33:48 +0200

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
+    css: ["~/assets/css/main.css"],
     modules: [
         "@nuxt/image-edge",
     ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
